374fd396ede4500fda18cc26e1c11be89afa5c93
William Guo <shaowei3608@gmail.com>
2015-06-24 17:24:24 +0800

    新界面和图标

--- build/generated/source/r/debug/com/william/howold/R.java
+++ build/generated/source/r/debug/com/william/howold/R.java
@@ -11,21 +11,37 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int female=0x7f020000;
-        public static final int hint=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int male=0x7f020003;
-        public static final int t4=0x7f020004;
+        public static final int bg_button=0x7f020000;
+        public static final int bg_main=0x7f020001;
+        public static final int female=0x7f020002;
+        public static final int hint=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int male=0x7f020005;
+        public static final int shape=0x7f020006;
+        public static final int t4=0x7f020007;
+        public static final int t5=0x7f020008;
+        public static final int user=0x7f020009;
     }
     public static final class id {
+        public static final int ImageView01=0x7f060007;
+        public static final int LinearLayoutAdMob=0x7f06000b;
+        public static final int TextView01=0x7f060005;
         public static final int detect=0x7f060001;
         public static final int getImg=0x7f060002;
         public static final int id_age_gender=0x7f060004;
+        public static final int imageSelect=0x7f06000c;
         public static final int imgArea=0x7f060000;
+        public static final int returns=0x7f060008;
+        public static final int save=0x7f060009;
+        public static final int select=0x7f06000d;
+        public static final int share=0x7f06000a;
+        public static final int textView1=0x7f060006;
         public static final int waiting=0x7f060003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_share=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
